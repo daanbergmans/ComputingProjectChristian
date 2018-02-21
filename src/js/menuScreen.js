7933587868e1aeb9a946c0a6d8babf1ad681dcf0
Daan <bergmansdaan@live.be>
2018-02-21 15:54:25 +0100

    Cleanup + basic navigation

---
+++ src/js/menuScreen.js
@@ -0,0 +1,11 @@
+function navToScores() {
+  window.location.href = "../html/playScreen.html";
+}
+
+function navToPlay() {
+  window.location.href = "../html/playScreen.html";
+}
+
+function navToSettings() {
+  window.location.href = "../html/settingsScreen.html";
+}
