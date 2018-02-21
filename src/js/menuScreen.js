1b7ad2421262ec0049b16dc0c9b8bb8f23491fd9
Daan <bergmansdaan@live.be>
2018-02-21 16:29:58 +0100

    Screens

--- src/js/menuScreen.js
+++ src/js/menuScreen.js
@@ -1,11 +1,11 @@
 function navToScores() {
-  window.location.href = "../html/playScreen.html";
+  location.replace("../html/playScreen.html");
 }
 
 function navToPlay() {
-  window.location.href = "../html/playScreen.html";
+  location.replace("../html/playScreen.html");
 }
 
 function navToSettings() {
-  window.location.href = "../html/settingsScreen.html";
+  location.replace("../html/settingsScreen.html");
 }
