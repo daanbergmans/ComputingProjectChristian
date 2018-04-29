ed6b121d4965111808e931108436c051c5427341
Daan Bergmans <bergmansdaan@live.be>
2018-04-29 15:52:33 +0200

    Small bugfix

--- src/js/playScreen.js
+++ src/js/playScreen.js
@@ -1,6 +1,7 @@
 document.onkeydown = checkKey;
 
 let currentPosition = "0607";
+let newPosition;
 
 function fillTable() {
   let table = document.getElementById("playField");
@@ -30,7 +31,7 @@ function checkKey(e) {
     break;
     case 38: moveUp();
     break;
-    case 39: moveRigh();
+    case 39: moveRight();
     break;
     case 40: moveDown();
     break;
@@ -38,19 +39,36 @@ function checkKey(e) {
 
 }
 
-function moveUp() {
-  let newPosition;
+function moveLeft() {
+  if (currentPosition.substring(2, 4) == "00") {
+    return;
+  }
 
+
+}
+
+function moveUp() {
   if (currentPosition.substring(0, 2) == "00") {
     return;
   }
 
   if (currentPosition.substring(0, 1) == "0") {
-    console.log(currentPosition.substring(0, 1));
-    console.log(currentPosition.substring(2, 4));
-    newPosition = "0" + (parseInt(currentPosition.substring(1, 2), 10) - 1) + "" + currentPosition.substring(2, 4);
-    document.getElementById(currentPosition).innerHTML = "";
-    document.getElementById(newPosition).innerHTML = "test";
-    currentPosition = newPosition;
+    newPosition = "0" + (parseInt(currentPosition.substring(1, 2), 10) - 1);
+  } else {
+    newPosition = (parseInt(currentPosition.substring(0, 2), 10) - 1);
   }
+
+  newPosition += "" + currentPosition.substring(2, 4);
+
+  document.getElementById(currentPosition).innerHTML = "";
+  document.getElementById(newPosition).innerHTML = "test";
+  currentPosition = newPosition;
+}
+
+function moveRight() {
+
+}
+
+function moveDown() {
+
 }
