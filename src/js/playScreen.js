780e9972e1db7e40395acc100c203aeaf3b85041
Daan Bergmans <bergmansdaan@live.be>
2018-04-29 19:32:57 +0200

    Bug fix, but created bug ResidentSleeper

--- src/js/playScreen.js
+++ src/js/playScreen.js
@@ -1,7 +1,7 @@
 document.onkeydown = checkKey;
 
 let currentPosition = "0607";
-let newPosition;
+let newPosition = "0607";
 
 function fillTable() {
   let table = document.getElementById("playField");
@@ -26,6 +26,8 @@ function fillTable() {
 function checkKey(e) {
   e = e || window.event;
 
+  console.log("Current position: " + currentPosition);
+
   switch(e.keyCode) {
     case 37: moveLeft();
     break;
@@ -36,6 +38,8 @@ function checkKey(e) {
     case 40: moveDown();
     break;
   }
+
+  console.log("Current position: " + currentPosition);
 
 }
 
@@ -80,10 +84,10 @@ function moveRight() {
     return;
   }
 
-  if (currentPosition.substring(2, 3) == "0") {
+  if (currentPosition.substring(2, 3) == "0" && parseInt(currentPosition.substring(3, 4), 10) < 9) {
     newPosition = "0" + (parseInt(currentPosition.substring(3, 4), 10) + 1);
-  } else {
-    newPosition = (parseInt(currentPosition.substring(0, 2), 10) + 1);
+  } else if (currentPosition.substring(2, 3) == "1" || currentPosition.substring(3, 4) == "9") {
+    newPosition = (parseInt(currentPosition.substring(2, 4), 10) + 1).toString();
   }
 
   newPosition = currentPosition.substring(0, 2) + "" + newPosition;
@@ -91,17 +95,19 @@ function moveRight() {
   document.getElementById(currentPosition).innerHTML = "";
   document.getElementById(newPosition).innerHTML = "test";
   currentPosition = newPosition;
+  newPosition = "";
 }
 
 function moveDown() {
+
   if (currentPosition.substring(0, 2) == "12") {
     return;
   }
 
-  if (currentPosition.substring(0, 1) == "0") {
-    newPosition = "0" + (parseInt(currentPosition.substring(1, 2), 10) + 1);
-  } else {
-    newPosition = (parseInt(currentPosition.substring(0, 2), 10) + 1);
+  if (currentPosition.substring(0, 1) == "0" && parseInt(currentPosition.substring(1, 2), 10) < 9) {
+    newPosition = "0" + (parseInt(currentPosition.substring(1, 2), 10) + 1).toString();
+  } else if (currentPosition.substring(0, 1) == "1" || currentPosition.substring(1, 2) == "9") {
+    newPosition = (parseInt(currentPosition.substring(0, 2), 10) + 1).toString();
   }
 
   newPosition += "" + currentPosition.substring(2, 4);
